675d52c734cbb23c7a0bd13f6b5e1eb9f55679c9
shivnandakalaskar <shivnanda.kalaskar1@gmail.com>
2021-05-16 23:27:16 +0530

    [Shivnanda] Add : Star and alphabetical Triangle patterns

---
+++ patterns/alpha_pattern_1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+
+int i=0,j=0,pos=0,space=0;
+printf("Enter the position of rows : ");
+scanf("%d",&pos);
+for(i=1;i<=pos;i++)
+{	
+	printf("\n");
+	for(space=1;space<=(pos-i);space++)
+	{
+	printf(" ");
+	}
+	for(j=1;j<=(2*i)-1;j++)
+	{
+	printf("%c", j+96);
+	}
+
+}
+
+return 0;
+}
