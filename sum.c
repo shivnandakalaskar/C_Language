886a4d56947f0b0735c23d55b8cb9b2934e53134
shivnandakalaskar <shivnanda.kalaskar1@gmail.com>
2021-06-03 20:13:44 +0530

    [Shivnanda] Add: Sum of two numbers

---
+++ sum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+
+{
+int no_1=0,no_2=0,sum=0;
+
+printf("Enter two numbers to add: ");
+scanf("%d %d",&no_1,&no_2);
+
+sum=no_1+no_2;
+
+printf("Sum is %d \n",sum);
+
+return 0;
+
+}
