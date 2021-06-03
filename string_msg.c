9ee489ee77e8c1928a384536d727973a255a2799
shivnandakalaskar <shivnanda.kalaskar1@gmail.com>
2021-06-03 20:15:09 +0530

    [Shivnanda] Add: Printing message by using user name

---
+++ string_msg.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+
+char name[15];
+
+
+printf("Enter your Name: ");
+scanf("%s",name);
+
+printf("Welcome %s in C programing \n",name);
+
+return 0;
+
+}
+
+
+
+
+
+
